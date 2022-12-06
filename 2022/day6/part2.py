f5ecb49903824a906cec84f5054753cd57e07536
ksbidlack <ksbidlack@gmail.com>
2022-12-05 21:06:28 -0800

    2022-day6

---
+++ 2022/day6/part2.py
@@ -0,0 +1,11 @@
+with open('./input.txt') as file:
+    stuff = file.read()
+
+buf = []
+for index, char in enumerate(stuff):
+    if len(buf) == 14:
+        buf.pop(0)
+    buf.append(char)
+    if len(set(buf)) == len(buf):
+        print(buf, index)
+
